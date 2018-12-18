4413aa81a0769af8fa768b84c68fe7b6f5a28f88
Cody Maxie <38113810+codythegreat@users.noreply.github.com>
2018-12-18 16:21:53 -0600

    Slight changes to main.js

--- main.js
+++ main.js
@@ -1,10 +1,11 @@
 document.getElementById("nav-top").addEventListener(onmouseover, function() {
-    currentOpacity = 0;
-    let intervalIncreaseOpacityLeft = setInterval(function() {increaseOpacity("content-left")}, 10);
-    let intervalIncreaseOpacityRight = setInterval(function() {increaseOpacity("content-right")}, 10);
+    currentOpacity = 0.0;
+    let intervalIncreaseOpacityLeft = setInterval(function() {increaseOpacity("content-left")}, 30);
+    let intervalIncreaseOpacityRight = setInterval(function() {increaseOpacity("content-right")}, 30);
 
     const increaseOpacity = (domElement) => {
-        document.getElementById(domElement).style.opacity = currentOpacity;
+        document.getElementById("content-left").style.opacity = `${currentOpacity}`;
+        document.getElementById("content-right").style.opacity = `${currentOpacity}`;
         if (currentOpacity == 1) {
             currentOpacity = 0;
             clearInterval(intervalIncreaseOpacityLeft);
